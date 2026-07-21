ebf0b6705031f0346f56272731b4877958a6835b
ArturoZAC <arturoyz2105@gmail.com>
2026-07-20 20:05:40 -0500

    fix: initialize formula state and add useEffect for number updates

--- hooks/useCalculator.ts
+++ hooks/useCalculator.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 enum Operator {
   Add = "+",
@@ -8,14 +8,38 @@ enum Operator {
 }
 
 export const useCalculator = () => {
-  const [formula, setFormula] = useState("");
+  const [formula, setFormula] = useState("0");
   const [number, setNumber] = useState("0");
   const [prevNumber, setPrevNumber] = useState("0");
 
   const lastOperation = useRef<Operator>(null);
 
+  useEffect(() => {
+    setFormula(number);
+  }, [number]);
+
   const buildNumber = (numberString: string) => {
-    console.log({ numberString });
+    if (number.includes(".") && numberString === ".") return;
+
+    if (number.startsWith("0") || number.startsWith("-0")) {
+      if (numberString === ".") {
+        return setNumber(number + numberString);
+      }
+
+      if (numberString === "0" && number.includes(".")) {
+        return setNumber(number + numberString);
+      }
+
+      if (numberString !== "0" && !number.includes(".")) {
+        return setNumber(numberString);
+      }
+
+      if (numberString === "0" && !number.includes(".")) {
+        return;
+      }
+    }
+
+    setNumber(number + numberString);
   };
 
   return {
